e4b3b1699010c575ab993a5c3218d9ec4afda2a7
Gherasim Bogdan <bogdan.gherasim@student.manchester.ac.uk>
2017-11-14 15:59:10 +0000

    update

--- ex6/pangolins.c
+++ ex6/pangolins.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 
-typedef struct tree
+typedef struct Tree
 {
   tree *yes;
   tree *no;
@@ -17,6 +17,33 @@ void printNode()
     printf("Question: %s\nYes: %s\nNo: %s\n", nodes->name, nodes->yes->name,nodes->no->name);
   else
     printf("Object: %s\n", nodes->name);
+
+}
+
+Tree *newTree(char *name, question_or_object type)
+{
+  Tree *returnValue = malloc(sizeof(Tree));  
+  returnValue->name = name;
+  returnValue->type = type;
+  return returnValue;
+}
+
+void initTree()
+{
+  Tree pizza = makeObject("a pizza", question_or_object.OBJECT, NULL, NULL);
+  Tree monkey = makeObject("a monkey", question_or_object.OBJECT, NULL, NULL);
+  Tree cat = makeObject("a cat", question_or_object.OBJECT, NULL, NULL);
+  Tree isPizza = makeObject("Is it flat, round and edible?", pizza, )
+  Tree isMonkey = makeObject("Does it have a tail?", question_or_object.QUESTION, monkey, )
+
+}
+
+Tree makeObject(char *name, question_or_object type,char *yes, char *no,)
+{
+    *node = newTree(name,type);
+    *node->yes = newTree(yes,)
+    return node;
+
 
 }
 int main(int argc, char **argv) 
