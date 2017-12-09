02931074c5b76ddfbadaf6628f91732918a6ceb9
The best of the rest <thebestoftherest@mihais-mbp.home>
2017-12-09 17:58:25 +0000

    Ex3

--- ex7/elGamal.c
+++ ex7/elGamal.c
@@ -134,7 +134,7 @@ int main(int argc, char **argv)
 {
   // printf("%d\n", hcf(atoi(argv[1]), atoi(argv[2])));
   printf("%d\n", fme(5, atoi(argv[1]), 19));
-  // printf("%d\n", dl(8, 3, 17));
+  printf("%d\n", dl(fme(5, atoi(argv[1]), 19), 5, 19));
   // printf("%d\n", imp(8, 13));
 
   //doElGamal();
